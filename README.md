5d96269846affd0038ec75d115eee15baa067ab2
Asiri Hewage <foolmashi@gmail.com>
2019-07-23 22:34:52 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Memo---The-Memory-Meter
 Internship at Pearson, Task 02 | Memory meter to measure memory utilization on multiple servers. 
+
+###Client
+Client is developed using MDB React. It will call the server API continuously to get data. The client is running on node, so follow these steps to turn up the client. I recommend WebStorm as an IDE to build and run the client.
+
+Clone the project using git
+
+```bash
+git clone
+```
+Install dependencies
+```npm
+npm install
+```
+Run the script
+```npm
+npm start
+```
+
+
+![alt text](https://raw.githubusercontent.com/username/projectname/branch/path/to/img.png)
+
+###Server
+Server is running an API on python Flask environment.
+
+Install modules required using pip
+```python
+pip install -r Server/requirements.txt
+```
+
+Run python script
+```python
+python Server/server.py 
+```
+
+![alt text](https://raw.githubusercontent.com/userna13me/projectname/branch/path/to/img.png)
+
--- Server/server.py
+++ Server/server.py
@@ -1,23 +1,37 @@
-import schedule
-import time
+from __future__ import print_function
+from flask import Flask , jsonify
+import os
+import psutil
+import json
+import datetime
+from flask_cors import CORS
 
-def job():
-    import requests 
-      
-    server01 = "http://localhost:5000/api/analytics/memory"
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+pid = os.getpid()
+py = psutil.Process(pid)
 
-    r = requests.get(url = server01) 
-      
-    data = r.json() 
+@app.route('/api/analytics/memory', methods=['GET'])
+def get_tasks():
     
-    print(data)
-    
-schedule.every(1).minutes.do(job)
-#schedule.every().hour.do(job)
-#schedule.every().day.at("10:30").do(job)
-print("Monitoring Started")
-while 1:
-    
-    schedule.run_pending()
-    time.sleep(1)
+    hostname = "server 01"
+    timeStamp = datetime.datetime.now().timestamp()
 
+    virtual_memory = psutil.virtual_memory()
+    cpu_percent = psutil.cpu_percent()
+    data = jsonify(
+        serverId = hostname,
+        cpuPercentage = cpu_percent,
+        available = virtual_memory[1], 
+        free = virtual_memory[4], 
+        percent = virtual_memory[2], 
+        total = virtual_memory[0], 
+        used = virtual_memory[3],
+        unit = 'bytes',
+        timestamp = timeStamp
+        )
+    
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True)
